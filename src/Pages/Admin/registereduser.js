4206a9d05e877a65a035a209fbe0122ac8ebecb3
kevinjariwala13 <113717216+kevinjariwala13@users.noreply.github.com>
2024-03-17 22:53:47 +0530

    display registered users

--- src/Pages/Admin/registereduser.js
+++ src/Pages/Admin/registereduser.js
@@ -1,17 +1,33 @@
 import React, { useState, useEffect } from "react";
 
 function UsersTable() {
-  const [users, setUsers] = useState(null);
-
-  useEffect(() => {
-    async function fetchData() {
-      const response = await fetch("http://localhost:5000/user/get");
-      const jsonData = await response.json();
-      setUsers(jsonData.user);
-      console.log(jsonData);
-    }
-    fetchData();
-  }, []);
+ 
+  const [user,setUser] = useState([]);
+  useEffect(()=>{
+   fetchUsers();
+  })
+   function fetchUsers(){
+   fetch("http://localhost:5000/user/display/").then((response)=>{
+   
+   return response.json()
+   })
+   .then((data)=>{
+     console.log(data.data.data);
+     const transformUser = data.data.data.map(userData =>{
+       return {
+         fname: userData.firstname,
+         lname: userData.lastname,
+         address: userData.address,
+         contact: userData.contact,
+         email: userData.email
+ 
+       }
+     
+     });
+ 
+     setUser(transformUser);
+   })
+  }
 
   return (
     <table className="table">
@@ -22,19 +38,26 @@ function UsersTable() {
           <th>Address</th>
           <th>Contact</th>
           <th>Email</th>
+          {/* <th>Password</th> */}
         </tr>
       </thead>
       <tbody>
-        {users &&
-          users.map((user) => (
-            <tr key={user._id}>
-              <td>{user.firstname}</td>
-              <td>{user.lastname}</td>
-              <td>{user.address}</td>
-              <td>{user.contact}</td>
-              <td>{user.email}</td>
-            </tr>
-          ))}
+    {
+      user.map(e=>(
+        
+        <> 
+          <tr>
+            <td>{e.fname.charAt(0).toUpperCase() + e.fname.slice(1)}</td>
+            <td>{e.lname.charAt(0).toUpperCase()+ e.lname.slice(1)}</td>
+            <td>{e.address}</td>
+            <td>{e.contact}</td>
+            <td>{e.email}</td>
+        
+          </tr>
+          </>
+        ))
+        }
+        {/* ))} */}
       </tbody>
     </table>
   );
