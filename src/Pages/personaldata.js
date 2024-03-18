eb405ed35dbed031f953a3992ac8edbfce952ea8
kevinjariwala13 <113717216+kevinjariwala13@users.noreply.github.com>
2024-03-18 13:32:48 +0530

    personal data

--- src/Pages/personaldata.js
+++ src/Pages/personaldata.js
@@ -1,7 +1,36 @@
 import React from "react";
+import {useForm} from "react-hook-form";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const PersonalData = () => {
+  const {
+    formState: { errors },
+    handleSubmit,
+    register,
+    reset,
+  } = useForm();
+
+  const insertComplaint = async (data) => {
+    try {
+      const url = "http://localhost:5000/complaint/insert";
+      const details = {
+        fname: data.firstname,
+        lname: data.lastname,
+        email: data.email,
+        contactno: data.contactno,
+        address: data.address,
+        // idproof: data.identityproof,
+        // date: data.date,
+        
+      };
+      const res = await axios.post(url, { details },{withCredentials: true});
+      console.log(res);
+      reset();
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <>
       <div class="container mt-2 pt-2">
@@ -11,90 +40,106 @@ const PersonalData = () => {
           </div>
           <hr />
         </div>
-        <form class="row g-3 shadow py-4 px-4 mx-5 my-5 " id="complaintdetails">
+        <form class="row g-3 shadow py-4 px-4 mx-5 my-5 " id="complaintdetails" onSubmit={handleSubmit(insertComplaint)}>
           <div class="col-sm-12">
             <h2 class="text text-danger fw-3">Personal details</h2>
           </div>
           <hr />
           <div class="col-md-6">
-            <label for="FName" class="form-label">
+            <label for="firstname" class="form-label">
               First Name
             </label>
             <input
               type="text"
               class="form-control"
-              id="FName"
+              id="firstname"
               placeholder="First Name"
-              required
+              {...register("firstname",{
+                required:true,
+              })}
             />
           </div>
 
           <div class="col-md-6">
-            <label for="LName" class="form-label">
+            <label for="lastname" class="form-label">
               Last Name
             </label>
             <input
               type="text"
               class="form-control"
-              id="LName"
+              id="lastname"
               placeholder="Last Name"
-              required
+              {...register("lastname",{
+                required: true,
+              })}
             />
           </div>
 
           <div class="col-md-6">
-            <label for="inputEmail" class="form-label">
+            <label for="email" class="form-label">
               Email
             </label>
             <input
-              type="email"
+              type="text"
               class="form-control"
-              id="inputEmail"
+              id="email"
               placeholder="example@email.com"
-              required
+              {...register("email",{
+                required: true
+              })}
             />
           </div>
           <div class="col-md-6">
-            <label for="contact" class="form-label">
+            <label for="contactno" class="form-label">
               Contact No.
             </label>
             <input
               type="text"
               class="form-control"
-              id="contact"
+              id="contactno"
               placeholder="Contact Number"
-              required
+              {...register("contactno",{
+                required: true,
+              })}
             />
           </div>
           <div class="col-md-6">
             <label for="date" class="form-label">
               Date
             </label>
-            <input type="date" class="form-control" id="date" />
+            <input type="date" class="form-control" id="date"
+              {...register("date",{
+                required:true,
+              })}
+            />
           </div>
           <div class="col-md-6">
-            <label for="Identityproof" class="form-label">
+            <label for="idproof" class="form-label">
               Identity Proof(Adharcard/ Driving License/ PAN Card)
             </label>
             <input
               type="file"
               class="form-control"
-              id="identityproof"
+              id="idproof"
               placeholder="Upload here"
-              required
+              {...register("idproof",{
+                required: true,
+              })}
             />
           </div>
           <div class="col "></div>
           <div class="col-12">
-            <label for="inputAddress" class="form-label">
+            <label for="address" class="form-label">
               Address
             </label>
             <input
               type="text"
               class="form-control"
-              id="inputAddress"
+              id="address"
               placeholder="1234 Main St"
-              required
+              {...register("address",{
+                required: true,
+              })}
             />
           </div>
 
@@ -105,19 +150,19 @@ const PersonalData = () => {
               type="reset"
               id="btnclear"
               to=""
-              class=" btn btn-outline-danger btn-smshadow-sm    w-100 bg-gradient  p-3 text-center  fw-bold "
+              class="btn btn-smshadow-sm    w-100 bg-gradient  p-3 text-center  fw-bold "
             >
               Clear
             </button>
           </div>
           <div class="col-3 ">
-            <button
-              type="submit"
-              href="/Incident"
-              class=" btn btn-outline-danger shadow-sm button btn-sm  shadow-sm w-100 bg-gradient p-3 text-center  fw-bold "
-            >
-              <span>Next</span>
-            </button>
+            {/* <Link
+              to="/Incident"
+              class="btn shadow-sm button btn-sm  shadow-sm w-100 bg-gradient p-3 text-center  fw-bold "
+            > */}
+              {/* <span>Next</span> */}
+            {/* </Link> */}
+            <button type="submit"> Insert</button>
           </div>
           <div class="col-4 text-end"></div>
         </form>
