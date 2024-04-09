b373f005fe26776dd4d78ef04d1da0fe0e56ce66
pjari14 <prishajariwala14@gmail.com>
2024-04-09 17:55:41 +0530

    commit

--- src/Pages/PageNotFound.js
+++ src/Pages/PageNotFound.js
@@ -8,13 +8,13 @@ const PageNotFound = () => {
             <div class="text-center">
               <h2 class="d-flex justify-content-center align-items-center gap-2 mb-4">
                 <span class="display-1 fw-bold">4</span>
-                <i class="bi bi-exclamation-circle-fill text-danger display-4"></i>
+                <span class="display-1 fw-bold text-danger">0</span>
                 <span class="display-1 fw-bold bsb-flip-h">4</span>
               </h2>
               <h3 class="h2 mb-2">Oops! You're lost.</h3>
               <p class="mb-5">The page you are looking for was not found.</p>
               <a
-                class="btn bsb-btn-5xl btn-dark rounded-pill px-5 fs-6 m-0"
+                class="btn bsb-btn-5xl btn-dark text-light rounded-pill px-5 fs-6 m-0"
                 href="/Home"
                 role="button"
               >
